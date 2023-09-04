aa9fb2b8fcc59bf7798489515b2a2889efba4933
fjimiz <fjtmzk0723@icloud.com>
2023-09-04 19:55:51 +0900

    update

--- app/components/MainHome.tsx
+++ app/components/MainHome.tsx
@@ -1,5 +1,18 @@
-import { Box } from "@chakra-ui/react"
+"use client"
+import { Box, Stack } from "@chakra-ui/react"
+import { FC } from "react"
 
-export const MainHome = () => {
-  return <Box>foo</Box>
+type Props = {
+  results: { name: string }[]
+}
+
+export const MainHome: FC<Props> = (props) => {
+  console.log(props)
+  return (
+    <Stack>
+      {props.results.map((result) => (
+        <Box key={result.name}>{result.name}</Box>
+      ))}
+    </Stack>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image"
-import styles from "./page.module.css"
 import { MainHome } from "app/components/MainHome"
 
-const HomePage = () => {
-  return <MainHome />
+const HomePage = async () => {
+  const resp = await fetch("https://pokeapi.co/api/v2/pokemon")
+  const data = await resp.json()
+  console.log(data.results)
+  return <MainHome results={data.results} />
 }
 
 export default HomePage
