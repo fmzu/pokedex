c4c0e5006d3b9f7be5abacf3c2430854fd2e1e2e
fjimiz <fjtmzk0723@icloud.com>
2023-09-07 00:02:12 +0900

    画像の切り替えをする

--- app/components/BoxPokemon.tsx
+++ app/components/BoxPokemon.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { Box, Image } from "@chakra-ui/react"
-import { FC } from "react"
+import { Box, Image, Stack, Text } from "@chakra-ui/react"
+import { FC, useState } from "react"
 
 type Props = {
   id: number
@@ -8,11 +8,27 @@ type Props = {
 }
 
 export const BoxPokemon: FC<Props> = (props) => {
-  const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.id}.png`
+  const [isActive, setActive] = useState(false)
+
+  const imageURL = isActive
+    ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`
+    : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.id}.png`
+  console.log("aaa", isActive)
   return (
-    <Box>
-      <Image alt={props.name} src={imageURL} />
-      {props.name}
-    </Box>
+    <Stack spacing={0}>
+      <Box
+        onMouseEnter={() => {
+          setActive(true)
+        }}
+        onMouseLeave={() => {
+          setActive(false)
+        }}
+      >
+        <Image alt={props.name} src={imageURL} />
+      </Box>
+      <Text textAlign={"center"} fontWeight={"bold"} letterSpacing={1}>
+        {props.name}
+      </Text>
+    </Stack>
   )
 }
